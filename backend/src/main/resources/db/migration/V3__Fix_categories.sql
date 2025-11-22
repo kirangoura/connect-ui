fe9cabc379b01b9792871a7b90ec15123e337584
Kiran Gourabathina <kiran.goura@gmail.com>
2025-11-21 22:27:22 -0600

    Removed the backend code from UI

--- backend/src/main/resources/db/migration/V3__Fix_categories.sql
+++
@@ -1,3 +0,0 @@
--- Update all events to use only 3 categories: Events, Sports, Fitness
-UPDATE events SET category = 'Events' WHERE category = 'Social';
-UPDATE events SET category = 'Events' WHERE category = 'Gathering';
